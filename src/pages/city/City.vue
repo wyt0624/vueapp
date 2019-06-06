10e0f6d8470a587208d6828a5abbc01ebf2a26c0
wyt0624 <64537109@qq.com>
2019-06-06 17:09:58 +0800

    城市选择

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,13 +1,21 @@
 <template>
-    <city-header></city-header>
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
+        <city-list></city-list>
+    </div>
 </template>
 <script>
 import CityHeader from "./conponents/Header"
+import CitySearch from "./conponents/Search"
+import CityList from "./conponents/List"
 
 export default {
     name: "City",
     components: {
         CityHeader,
+        CitySearch,
+        CityList,
     }
 }
 </script>
